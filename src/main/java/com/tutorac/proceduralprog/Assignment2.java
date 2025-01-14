97768041a26bd72dba845b4c93cf26192f8c2fc7
SiddharthMishra28 <connectwithsiddharthm@gmail.com>
2025-01-14 09:51:35 +0530

    Data Types, conditional statements and loops

---
+++ src/main/java/com/tutorac/proceduralprog/Assignment2.java
@@ -0,0 +1,16 @@
+package com.tutorac.proceduralprog;
+
+public class Assignment2 {
+    // TV REMOTE SCEANRIO
+    // Display a menu to user with different button options
+    // 1. option 1 change view mode!
+    // 2. option 2
+    // 3. option 3
+    // 4. quit program - Program should terminate
+    // show appropriate sub menu
+    //        option 1 :
+    //               - suboption 1 : change brightness
+    //                             output - brightness changed!
+    //               - suboption 2 : change contrast
+    //               = suboption 3 : change color saturation
+}
