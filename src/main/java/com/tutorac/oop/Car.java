9f822178516b2836fd8d3775597da2b585ca8fb6
SiddharthMishra28 <connectwithsiddharthm@gmail.com>
2025-01-21 10:03:47 +0530

    Object Oriented Programming Basics

---
+++ src/main/java/com/tutorac/oop/Car.java
@@ -0,0 +1,26 @@
+package com.tutorac.oop;
+
+// BLUEPRINT OF A CAR
+public class Car {
+    String brand;
+    String model;
+    int numWheels;
+    String engineType;
+
+    public static void main(String[] args) {
+        // INSTANCE OF A CAR
+        Car tataNexon = new Car(); // new keyword is used to create an object instance
+                                   // also to reserve memory for it!
+        tataNexon.brand = "TATA";
+        tataNexon.model = "Nexon";
+        tataNexon.numWheels = 4;
+        tataNexon.engineType = "Diesel";
+
+        // INSTANCE OF A CAR
+        Car thar = new Car();
+        thar.brand = "Mahindra";
+        thar.model = "Thar";
+        thar.numWheels = 4;
+        thar.engineType = "Petrol";
+    }
+}
